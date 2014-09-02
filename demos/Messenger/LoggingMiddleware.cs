61cb694dfff6c7051eadceb45e7d3073795ed592
Adam Barney <adam@adambarney.com>
2014-09-02 01:29:00 -0500

    More detailed logging

--- demos/Messenger/LoggingMiddleware.cs
+++ demos/Messenger/LoggingMiddleware.cs
@@ -1,24 +1,49 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+using System;  
+using System.Diagnostics;  
+using System.Threading;  
+using System.Threading.Tasks;  
+using Microsoft.AspNet.Builder;  
 using Microsoft.AspNet.Http;
 
 namespace Messenger
 {
 	public class LoggingMiddleware
 	{
-		private RequestDelegate _next;
+	    RequestDelegate _next;
+	    int _requestCount;
 
-        public LoggingMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
+	    // called once when pipeline is built
+	    public LoggingMiddleware(RequestDelegate next)
+	    {
+	        _next = next;
+	    }
 
-        public async Task Invoke(HttpContext httpContext)
-        {
-            Console.WriteLine("Starting " + httpContext.Request.Path);
-            await _next(httpContext);
-            Console.WriteLine("Ending " + httpContext.Request.Path);
-        }
+	    // called once per request
+	    public async Task Invoke(HttpContext context)
+	    {
+	        var sw = new Stopwatch();
+	        sw.Start();
+	        var requestNumber = Interlocked.Increment(
+	            ref _requestCount);
+
+	        // request is incoming
+	        Console.WriteLine(string.Format(
+	            "#{0} incoming {1} {2}{3}{4}",
+	            requestNumber,
+	            context.Request.Method,
+	            context.Request.PathBase,
+	            context.Request.Path,
+	            context.Request.QueryString));
+
+	        // pass control to following components
+	        await _next(context);
+
+	        // call is unwinding
+	        Console.WriteLine(string.Format(
+	            "#{0} outgoing {1} {2}ms", 
+	            requestNumber,
+	            context.Response.StatusCode,
+	            sw.ElapsedMilliseconds));
+	    }
 	}
 }
